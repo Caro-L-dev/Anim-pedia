f8430ea6691572a62bcb7de81fe9a39f89133a1f
Caro-L-dev <liardcar@gmail.com>
2021-06-18 10:58:50 +0200

    :sparkles: Create Search component

---
+++ src/components/Search.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Search () {
+    return (
+       <section className="searchbox-wrap">
+           <input type="text" placeholder="Search for an anime..." className="searchbox" />
+       </section>
+    )
+}
+
+export default Search;
